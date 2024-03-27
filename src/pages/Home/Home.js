a4d30b4a8d5455b48c82dff33a7d2537b8167469
Abhiraj Shourya <45894478+abhirajshourya@users.noreply.github.com>
2024-03-27 10:32:46 -0400

    Refactor App.js to use SafeAreaView and add Home component

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+})
