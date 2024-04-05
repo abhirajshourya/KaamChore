8549595468c8645b2c645f6cea9655d9dcc745ca
Abhiraj Shourya <45894478+abhirajshourya@users.noreply.github.com>
2024-04-05 00:08:44 -0400

    Add user email to profile card and style logout button

--- src/pages/Home/Profile/Profile.js
+++ src/pages/Home/Profile/Profile.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import SafeAreaWithInsets from '../../../components/SafeAreaWithInsets/SafeAreaWithInsets';
-import { logout } from '../../../controllers/firebase-controller';
+import { auth, logout } from '../../../controllers/firebase-controller';
 import { useDispatch } from 'react-redux';
 import styles from '../../../styles/main';
 
 const Profile = () => {
   const dispatch = useDispatch();
+  const user = auth.currentUser.toJSON();
   return (
     <SafeAreaWithInsets>
       <View style={styles.headerContainer}>
         <Text style={styles.header}>Profile</Text>
       </View>
       <View style={styles.listContainer}>
+        <View style={styles.card}>
+          <Text>Email: {user.email}</Text>
+        </View>
         <TouchableOpacity
+          style={{
+            padding: 10,
+            borderRadius: 5,
+            alignItems: 'center',
+            borderColor: 'red',
+            borderWidth: 1,
+          }}
           onPress={async () => {
             try {
               await logout();
@@ -23,7 +34,13 @@ const Profile = () => {
             }
           }}
         >
-          <Text>Logout</Text>
+          <Text
+            style={{
+              color: 'red',
+            }}
+          >
+            Logout
+          </Text>
         </TouchableOpacity>
       </View>
     </SafeAreaWithInsets>
